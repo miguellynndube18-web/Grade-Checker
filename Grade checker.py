ab48909b2a339b334b3462e30ff4c0688b1bd529
miguellynndube18-web <miguellynndube18@gmail.com>
2026-07-03 14:19:59 +0200

    Add files via upload

---
+++ Grade checker.py
@@ -0,0 +1,12 @@
+marks= input("Enter your marks") 
+marks= int(marks)
+if marks >= 90:
+    print("grade A")
+elif marks >= 75:
+    print("grade B") 
+elif marks >= 50:
+    print("grade C") 
+elif marks >= 30:
+    print("grade D")
+else:
+    print("fail")              
